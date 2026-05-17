b6c82c96709e6d0b36ee7f70236b0b4f83795e19
Viktor <viktor@example.com>
2026-05-17 14:52:01 +0300

    fix: add media URL configuration for DEBUG mode

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from catalog import views
 
@@ -26,3 +28,6 @@ urlpatterns = [
     path("contacts/", views.contacts, name="contacts"),
     path("catalog/", views.catalog, name="catalog"),  # Эту строку добавить
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
