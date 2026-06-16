0784169b9c3e9c80218f28222eba0ff39082b6a8
Viktor <viktor@example.com>
2026-06-16 17:06:59 +0300

    feat: fix auth redirects and logout view

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+
 from .forms import CustomAuthenticationForm
 from .views import RegisterView
 
@@ -7,10 +8,9 @@ app_name = "users"
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path("login/", LoginView.as_view(template_name="registration/login.html"), name="login"),
-    path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
     path("login/", LoginView.as_view(
         template_name="registration/login.html",
         authentication_form=CustomAuthenticationForm
     ), name="login"),
+    path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,28 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
+from django.contrib.auth import login
+from django.core.mail import send_mail
 from .forms import CustomUserCreationForm
 
 
 class RegisterView(CreateView):
     form_class = CustomUserCreationForm
     template_name = "registration/register.html"
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("users:login")
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        self.send_welcome_email(user.email)
+        return super().form_valid(form)
+
+    def send_welcome_email(self, user_email):
+        subject = "Добро пожаловать в наш сервис!"
+        message = "Спасибо за регистрацию. Мы рады вас видеть!"
+        send_mail(
+            subject,
+            message,
+            "viktor.britkin84@yandex.ru",
+            [user_email],
+            fail_silently=False,
+        )
