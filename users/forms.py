75251c839638ec16b06e7ffc0357851627b8da3f
Viktor <viktor@example.com>
2026-06-16 16:35:50 +0300

    feat: add auth templates with bootstrap styling (login, register)

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django import forms
 from .models import User
 
 
@@ -7,3 +7,18 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("email", "phone", "avatar", "country", "password1", "password2")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name == "avatar":
+                field.widget.attrs.update({"class": "form-control"})
+            else:
+                field.widget.attrs.update({"class": "form-control"})
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs.update({"class": "form-control"})
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-
+from .forms import CustomAuthenticationForm
 from .views import RegisterView
 
 app_name = "users"
@@ -9,4 +9,8 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
+    path("login/", LoginView.as_view(
+        template_name="registration/login.html",
+        authentication_form=CustomAuthenticationForm
+    ), name="login"),
 ]
