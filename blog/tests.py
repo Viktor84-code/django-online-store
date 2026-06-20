3ce3ef08b3442fda54fb65ca1ad6193a891d78b5
Viktor <viktor@example.com>
2026-06-20 13:18:01 +0300

    test: update blog tests for content manager permissions

--- blog/tests.py
+++ blog/tests.py
@@ -20,6 +20,14 @@ class BlogViewsTest(TestCase):
     def setUp(self):
         self.published_post = BlogPost.objects.create(title="Опубликованная статья", content="Текст", is_published=True)
         self.unpublished_post = BlogPost.objects.create(title="Черновик", content="Текст", is_published=False)
+        group, created = Group.objects.get_or_create(name='Контент-менеджер')
+        if created:
+            from django.contrib.contenttypes.models import ContentType
+            from django.contrib.auth.models import Permission
+            content_type = ContentType.objects.get_for_model(BlogPost)
+            permissions = Permission.objects.filter(content_type=content_type)
+            group.permissions.set(permissions)
+            group.save()
 
     def test_blog_list_shows_only_published(self):
         response = self.client.get(reverse("blog:list"))
@@ -48,17 +56,33 @@ class BlogViewsTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_blog_edit_page(self):
-        User.objects.create_user(email="test@test.com", password="testpass123")
+        # Обычный пользователь - доступ запрещен
+        user = User.objects.create_user(email="test@test.com", password="testpass123")
         self.client.login(email="test@test.com", password="testpass123")
-        post = BlogPost.objects.create(title="Test", content="Test content")
-        response = self.client.get(f"/blog/{post.pk}/update/")
+        response = self.client.get(f"/blog/{self.published_post.pk}/update/")
+        self.assertEqual(response.status_code, 403)
+
+        # Контент-менеджер - доступ разрешен
+        manager = User.objects.create_user(email="manager@test.com", password="testpass123")
+        group = Group.objects.get(name='Контент-менеджер')
+        manager.groups.add(group)
+        self.client.login(email="manager@test.com", password="testpass123")
+        response = self.client.get(f"/blog/{self.published_post.pk}/update/")
         self.assertEqual(response.status_code, 200)
 
     def test_blog_delete_page(self):
-        User.objects.create_user(email="test@test.com", password="testpass123")
+        # Обычный пользователь - доступ запрещен
+        user = User.objects.create_user(email="test@test.com", password="testpass123")
         self.client.login(email="test@test.com", password="testpass123")
-        post = BlogPost.objects.create(title="Test", content="Test content")
-        response = self.client.get(f"/blog/{post.pk}/delete/")
+        response = self.client.get(f"/blog/{self.published_post.pk}/delete/")
+        self.assertEqual(response.status_code, 403)
+
+        # Контент-менеджер - доступ разрешен
+        manager = User.objects.create_user(email="manager@test.com", password="testpass123")
+        group = Group.objects.get(name='Контент-менеджер')
+        manager.groups.add(group)
+        self.client.login(email="manager@test.com", password="testpass123")
+        response = self.client.get(f"/blog/{self.published_post.pk}/delete/")
         self.assertEqual(response.status_code, 200)
 
 
